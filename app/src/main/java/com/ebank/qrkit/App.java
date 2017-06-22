47143253979ec3081145c0ef2d2e859c954b0b1a
yuhongwei <13167582311@163.com>
2017-06-22 11:18:34 +0800

    init project

---
+++ app/src/main/java/com/ebank/qrkit/App.java
@@ -0,0 +1,14 @@
+package com.ebank.qrkit;
+
+import android.app.Application;
+
+/**
+ * 二维码项目的Application
+ *
+ * @author: hongweiyu
+ * @e_mail: yuxinburendavie@gmail.com
+ * @date: 2017/6/22
+ */
+
+public class App extends Application {
+}
---
+++ app/src/main/java/com/ebank/qrkit/Constants.java
@@ -0,0 +1,12 @@
+package com.ebank.qrkit;
+
+/**
+ * 二维码项目常量工具类
+ *
+ * @author: hongweiyu
+ * @e_mail: yuxinburendavie@gmail.com
+ * @date: 2017/6/22
+ */
+
+public class Constants {
+}
